2e4c61e11224282f0ac68e4b642b32e9b228c67f
kimo1261990 <hakim.kh@hotmail.com>
2013-02-24 06:04:09 -0800

    Inscription utilisateur finie

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,22 @@ class UsersController < ApplicationController
   @user = User.find(params[:id])
   @title = @user.nom
   end
-
+  
   def new
+  @user = User.new
   @title = "S'inscrire"
   end
+
+  def create
+  	@user =User.new(params[:user])
+  	if @user.save
+  		#traite un sucées d'enregistrement
+  		 redirect_to @user
+  		 flash[:success] = "Bienvenue dans SecuriSocial!"
+  	else
+  		@title = "inscripton"
+  		render 'new'
+  	end
+  end	
+
 end
